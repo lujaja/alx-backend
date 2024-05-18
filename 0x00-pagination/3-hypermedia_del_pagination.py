1652ce9bcc89f8a5921470ec06a75805f779c5ad
lujaja <jarzcyber@gmail.com>
2024-05-18 16:52:42 +0300

    correction

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -40,6 +40,9 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> dict:
+        """
+        Returns a dictionary containing the following key-value pairs:
+        """
         assert isinstance(index, int) and index >= 0
         assert isinstance(page_size, int) and page_size > 0
 
