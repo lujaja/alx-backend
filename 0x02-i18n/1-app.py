588a49de538d45832dce26492c69ff7eb2acc8fb
lujaja <jarzcyber@gmail.com>
2024-05-28 10:53:42 +0300

    get_locale

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,6 +6,7 @@ from flask_babel import Babel, _
 
 
 app = Flask(__name__)
+babel = Babel(app)
 
 
 class Config:
@@ -16,8 +17,6 @@ class Config:
 
 
 app.config.from_object(Config)
-
-babel = Babel(app)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
