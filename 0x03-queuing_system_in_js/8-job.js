cc0beec9bcda418d4ce53703d3880ed1c1fd23be
lujaja <jarzcyber@gmail.com>
2024-06-26 14:29:36 +0300

    function-10

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,28 @@
+import kue from 'kue';
+
+/**
+ * Creates push notification jobs and adds them to the queue
+ * @param {Array} jobs - Array of job objects
+ * @param {Object} queue - Kue queue
+ */
+const createPushNotificationsJobs = (jobs, queue) => {
+  if (!Array.isArray(jobs)) {
+    throw new Error('Jobs is not an array');
+  }
+
+  jobs.forEach((jobData) => {
+    const job = queue.create('push_notification_code_3', jobData).save((err) => {
+      if (!err) console.log(`Notification job created: ${job.id}`);
+    });
+
+    job.on('complete', () => {
+      console.log(`Notification job ${job.id} completed`);
+    }).on('failed', (err) => {
+      console.log(`Notification job ${job.id} failed: ${err}`);
+    }).on('progress', (progress) => {
+      console.log(`Notification job ${job.id} ${progress}% complete`);
+    });
+  });
+};
+
+export default createPushNotificationsJobs;
